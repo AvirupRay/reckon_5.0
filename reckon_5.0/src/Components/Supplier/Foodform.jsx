9c74621315f8048623198877a39cfc6fd08b726d
Roushan1512 <roxie4269@gmail.com>
2024-01-24 18:03:17 +0530

    object initialized

--- reckon_5.0/src/Components/Supplier/Foodform.jsx
+++ reckon_5.0/src/Components/Supplier/Foodform.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 const Foodform = ({ Foodlist, setFoodlist }) => {
-  const [Food, setFood] = useState({});
+  const [Food, setFood] = useState({ name: "", quantity: 0, date: "" });
   return <div>This is Input form</div>;
 };
 
