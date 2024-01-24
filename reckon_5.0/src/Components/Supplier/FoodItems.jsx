0599b4518d555b1beed4191b53921a897ac1b81e
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-01-25 00:10:22 +0530

    gradient-added

--- reckon_5.0/src/Components/Supplier/FoodItems.jsx
+++ reckon_5.0/src/Components/Supplier/FoodItems.jsx
@@ -15,21 +15,21 @@ const FoodItems = ({ Foodlist }) => {
   return (
     <>
       <div className=" px-10 py-10 h-full ">
-        <div className=" [background:radial-gradient(125%_125%_at_50%_10%,#f3ffdf_40%,#beaeae_100%)] inset-0 px-9 pt-9 rounded-lg h-full relative backdrop-blur">
+        <div className=" [background:radial-gradient(125%_125%_at_50%_10%,#e0ebbe_40%,#b9d1ae_100%)] inset-0 px-9 pt-9 rounded-lg h-full relative  shadow-inner backdrop-blur">
           <div className=" absolute top-[-10%] left-[3%] bg-[#6A7A50] text-white px-[2vw] py-[1vw] rounded-md text-l font-semibold  drop-shadow-md ">
             ALL ITEMS
           </div>
           <div className=" absolute top-[-10%] right-[3%] bg-[#8AA55C] text-white px-[2vw] py-[.5vw] rounded-md text-l font-semibold flex w-3/5 justify-evenly items-center  drop-shadow-md">
-            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-80">
+            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-80 cursor-pointer">
               23
             </div>
-            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-85">
+            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-85 cursor-pointer">
               22
             </div>
-            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-90">
+            <div className=" bg-[#e9e9e9] px-[.65vw] rounded-xl text-[#4C5A35] py-[.5vw] opacity-90 cursor-pointer">
               21
             </div>
-            <div className=" bg-[#e9e9e9] px-[2.2vw] rounded-2xl text-[#4C5A35] py-[.5vw]">
+            <div className=" bg-[#e9e9e9] px-[2.2vw] rounded-2xl text-[#4C5A35] py-[.5vw] cursor-pointer">
               TODAY
             </div>
           </div>
@@ -42,9 +42,7 @@ const FoodItems = ({ Foodlist }) => {
           )}
           <ul className="  h-full overflow-y-scroll scrollbar-hide  scroll-smooth">
             {Foodlist.map((dish) => (
-              <li className=" bg-[#fdfdfd] rounded-xl my-5 px-5 flex py-[3%] justify-around border-2 border-[#adadad]">
-                {/* <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-                <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div> */}
+              <li className=" bg-[#fdfdfd] rounded-xl my-5 px-5 flex py-[3%] justify-around drop-shadow">
                 <span className=" flex-1">{dish.name}</span>
                 <span className=" flex-1">Quantity: {dish.quantity}</span>
                 <span className=" flex-1">Best Before: {dish.date}</span>
