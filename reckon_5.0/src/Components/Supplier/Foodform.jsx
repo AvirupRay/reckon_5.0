6d8dc21bce69a8cc7660e848c31b8279334cb54d
Roushan1512 <roxie4269@gmail.com>
2024-01-24 18:45:04 +0530

    textboxes edited

--- reckon_5.0/src/Components/Supplier/Foodform.jsx
+++ reckon_5.0/src/Components/Supplier/Foodform.jsx
@@ -3,22 +3,37 @@ import { useState } from "react";
 
 const Foodform = ({ Foodlist, setFoodlist }) => {
   const [name, setName] = useState("");
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState("");
   const [date, setDate] = useState("");
   function submitted(e) {
     e.preventDefault();
-    setFoodlist([...Foodlist, { name: name, quantity: quantity, date: date }]);
+    setFoodlist([{ name: name, quantity: quantity, date: date }, ...Foodlist]);
+    setName("");
+    setQuantity("");
+    setDate("");
   }
   return (
     <div>
       <form onSubmit={submitted}>
-        <input onChange={(e) => setName({ name: e.target.value })} />
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          placeholder="Name of the Food"
+        />
         <br />
         <br />
-        <input onChange={(e) => setQuantity({ quantity: e.target.value })} />
+        <input
+          onChange={(e) => setQuantity(e.target.value)}
+          value={quantity}
+          placeholder="Quantity"
+        />
         <br />
         <br />
-        <input onChange={(e) => setDate({ date: e.target.value })} />
+        <input
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+          placeholder="Expiry Date"
+        />
         <br />
         <br />
         <button type="submit" className="btn">
