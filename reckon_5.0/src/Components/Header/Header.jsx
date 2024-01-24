c545b38fc7da2402ce296fc5f77f09fd2cf8a989
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-01-24 17:56:59 +0530

    header-changed

--- reckon_5.0/src/Components/Header/Header.jsx
+++ reckon_5.0/src/Components/Header/Header.jsx
@@ -3,8 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <NavLink to="/supplr">supp</NavLink>
+    <div className=" bg-[#dbd7b4] px-5 py-2 flex justify-around">
+      <NavLink
+        to="/supplr"
+        className={() =>
+          ` bg-[#ebebeb] px-2 py-1 rounded-md shadow-md font-semibold hover:bg-[#fff5ca] transition-colors`
+        }
+      >
+        Supplier's Page
+      </NavLink>
     </div>
   );
 };
